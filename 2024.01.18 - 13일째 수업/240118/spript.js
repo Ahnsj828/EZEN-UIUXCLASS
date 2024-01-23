61ac37b9ecc8be112e4cedc4b34d6786ee91c89a
ala828@naver.com <ala828@naver.com>
2024-01-23 09:25:39 +0900

    240123

--- 2024.01.18 - 13일째 수업/240118/spript.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".right");
-console.log(btn);
-// uerySelector 뭔가를 찾는다
-// btn이라는 변수에 담아줘
-
-btn.addEventListener("click", () => {
-  alert("오늘은 즐거운 목요일!");
-});
-// addEventListener가 btn한테 이벤트 줄거야
