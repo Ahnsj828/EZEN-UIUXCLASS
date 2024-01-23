41535607a16c947744828f74b82bc2c310653965
Ahnsj828 <ala828@naver.com>
2024-01-23 17:43:46 +0900

    240123

--- 2024.01.23-16day/script.js
+++ 2024.01.23-16day/script.js
@@ -2,21 +2,31 @@
 const trigger = document.querySelector(".trigger");
 // trigger라는 class를 찾아줘
 const gnb = document.querySelector("#gnb");
-const headfer = document.querySelector("header");
+const header = document.querySelector("header");
 // header라는 태그의 노드를 불러와서
-
+const gototop = document.querySelector(".gototop");
+// console.log(gototop):
+// ctrl shift J
 
 window.addEventListener("scroll", () => {
   // console.log(window.scrollY); 좌표알아보려고 쓴 코드
   if(window.scrollY>455) {
     headfer.classList.add("active");
-  }else(
+    gototop.classList.add("active");
+  } else (
     header.classList.remove("active");
+    gototop.classList.remove("active");
    )
   });
+  // if(~하다면)랑 else(~하지 않다면)는 짝꿍
 
 trigger.addEventListener("click", () => {
   gnb.classList.toggle("active");
   trigger.classList.toogle("active");
 });
 // 클릭 이벤트를 부여하겠다
+
+gototop.addEventListener("click", (e)=>{
+  e.preventDefault();
+  window.scrollTo({top:header, behavior:"smooth" })
+})
