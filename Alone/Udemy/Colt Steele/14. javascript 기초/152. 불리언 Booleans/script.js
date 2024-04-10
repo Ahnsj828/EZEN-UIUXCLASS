ef8cb14b9a1b1d19e432b35a92c5c2591f44f3c6
Ahnsj828 <ala828@naver.com>
2024-04-11 02:04:35 +0900

    JavaScript basic

---
+++ Alone/Udemy/Colt Steele/14. javascript 기초/152. 불리언 Booleans/script.js
@@ -0,0 +1,18 @@
+// < Booleans >
+// 불리언에는 두개의 값이 있다 => true와 faulse
+// 불리언은 예/ 아니오 나 참/거짓 값에 사용된다
+
+// let isLoggenIn = true;
+// let gameOver = false;
+// const isWaterWet = true;
+
+// let isActiveGame = false;
+// let isGameOver = true;
+
+// 
+// 
+
+// < Variables Can Change Taypes > JS에서는 type을 변경하는 변수를 가질 수 있다
+let numPuppies = 23  //  Number
+numPuppies = false  //  Now a Boolean
+numPuppies = 100  //  Back to Number
