b123ba92c39b2a320b7da4881242131cf0dde68a
Ahnsj828 <ala828@naver.com>
2024-04-27 22:46:38 +0900

    240427

---
+++ Alone/nomad/2_9 Recap/scritp.js
@@ -0,0 +1,23 @@
+// < 변수 >
+const variable = "anything"
+
+// < 배열 >
+const array = ["a", "b", "c", "d"]
+// array.push("e");
+
+// < 객체 >
+const object = {
+  name: "ahn",
+  age: 10,
+  fat: true,
+}
+// console.log(object.name); // ahn
+// console.log(object["name"]); //ahn
+// object.gender = "female"
+// console.log(object) // {name: 'ahn', age: 10, fat: true, gender: 'female'}
+
+// < 함수 >
+function functionA(argument1, argument2) {
+  console.log(argument1 + argument2)
+}
+functionA(3, 10)
