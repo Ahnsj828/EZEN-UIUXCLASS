78f951b3ae1d4a28d81b3821015d03cfef2425d9
Ahnsj828 <ala828@naver.com>
2024-05-07 18:37:36 +0900

    240507

---
+++ 포폴/1_tab menu/tab_menu01/script.js
@@ -0,0 +1,11 @@
+const tabBtns = document.querySelectorAll(".tab-btn a")
+console.log(tabBtns)
+
+tabBtns.forEach((tabBtn) => {
+  tabBtn.addEventListener("click", function () {
+    tabBtns.forEach((otherBtn) => {
+      otherBtn.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
