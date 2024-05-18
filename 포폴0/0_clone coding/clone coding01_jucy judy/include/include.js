abbdf7ba334bcb98e12ce6da11e9032be4ae996a
Ahnsj828 <ala828@naver.com>
2024-05-19 02:44:57 +0900

    240518

--- 포폴0/0_clone coding/clone coding01_jucy judy/include/include.js
+++ 포폴0/0_clone coding/clone coding01_jucy judy/include/include.js
@@ -1,21 +1,49 @@
-const headerMain = document.getElementById("header-main")
-const menuBtn = document.getElementById("menuBtn")
-const headerMenu = document.getElementById("btn-menu")
-const closeBtn = document.getElementById("closeBtn")
-// console.log(menuBtn, closeBtn)
+// // const headerMain = document.getElementById("header-main")
+// // const menuBtn = document.getElementById("menuBtn")
+// // const headerMenu = document.getElementById("btn-menu")
+// // const closeBtn = document.getElementById("closeBtn")
+// // // console.log(menuBtn, closeBtn)
 
-// function menuOpen() {
-//   menuBtn.addEventListener("click" , () =>{
-//     console.log("click")
-//   })
-// }
-menuBtn.addEventListener("click" , () =>{
-  // console.log("click")
-  headerMain.style.display = "none"
-  headerMenu.style.display = "block"
-})
-closeBtn.addEventListener("click" , () =>{
-  // console.log("click")
-  headerMain.style.display = "block"
-  headerMenu.style.display = "none"
-})
+// // // function menuOpen() {
+// // //   menuBtn.addEventListener("click" , () =>{
+// // //     console.log("click")
+// // //   })
+// // // }
+// // menuBtn.addEventListener("click" , () =>{
+// //   // console.log("click")
+// //   headerMain.style.display = "none"
+// //   headerMenu.style.display = "block"
+// // })
+// // closeBtn.addEventListener("click" , () =>{
+// //   // console.log("click")
+// //   headerMain.style.display = "block"
+// //   headerMenu.style.display = "none"
+// // })
+
+
+
+// // Get the modal
+// const menu = document.getElementById("menu");
+
+// // Get the button that opens the modal
+// const menuBtn = document.getElementById("menuBtn");
+
+// // Get the <span> element that closes the modal
+// const closeBtn = document.getElementsByClassName("closeBtn");
+
+// // When the user clicks the button, open the modal
+// menuBtn.addEventListener('click', () => {
+//   menu.style.display = 'block';
+// });
+
+// // When the user clicks on <span> (x), close the modal
+// closeBtn.addEventListener('click', () => {
+//   menu.style.display = 'none';
+// });
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.addEventListener('click', (event) => {
+//   if (event.target === modal) {
+//     menu.style.display = 'none';
+//   }
+// });
---
+++ 포폴0/0_clone coding/clone coding01_jucy judy/script.js
@@ -0,0 +1,10 @@
+// let headerIframe = document.getElementById("header-iframe");
+
+// headerIframe.onload = function () {
+//   headerIframe.style.height = headerIframe.contentWindow.document.body.scrollHeight + "px";
+// };
+
+// let footerIframe = document.getElementById("footer-iframe")
+// footerIframe.onload = function () {
+//   footerIframe.style.height = footerIframe.contentWindow.document.body.height + "px"
+// }
