3478ba0b1edb1b5589336bfef9982e219874cab6
Ahnsj828 <ala828@naver.com>
2024-06-08 19:09:17 +0900

    240608

---
+++ Alone/0604- 28.AJAX와 API/290_APIs 개요/script.js
@@ -0,0 +1,29 @@
+// JavaScript로 AJAX 요청을 할 때
+// HTML, CSS, JavaScript 가 아닌 순수한 정보를 원한다면? 코드가 아닌 순수 데이터가 필요하다면?
+//  ==> 이때 필요한게 API다
+// Application Programming Interface
+
+// 응용 프로그램 프로그래밍 인터페이스. 프로그래밍에서, 프로그램을 작성하기 위한 일련의 부(Sub) 프로그램, 프로토콜 등을 정의하여 상호 작용을 하기 위한 인터페이스 사양을 말한다.
+
+// API는 컴퓨터가 여러 소프트웨어와 상호 작용하거나 소통하는 모든 인터페이스를 의미하는 광범위한 용어다.
+// 오히려 소프트웨어끼리 작용하는 인터페이스라고 할 수 있다.
+
+// 꼭 컴퓨터일 필요는 없다. 일반적으로 웹과는 상관이 없다. 하지만 웹 개발자들이 API란 용어를 사용할 때는
+// 대부분 WebAPI를 뜻한다. WebAPI는 웹, HTTP를 기반으로 하는 인터페이스다.
+
+// 기업을 예로 들면 Twitter, Facebook이나 날씨나 비트코인도 API를 이용한다. 모두 특정 엔드포인트를 제공하고 제공되는 엔드포인트는 사용되는 코드에 정보로 응답하거나 다른 소프트웨어에 정보로 응답한다.
+
+// WebAPI는 다른 애플리케이션이나 데이터베이스로 가는 입구인 거다. 어딘가 있는 데이터 세트나 기업의 정보로 들어간다.
+// WebAPI라는 건 HTTP에서 일어나는 인터페이스란 걸 잊지 마라
+
+// 셀 수 없이 많은 API가 있다.
+
+// Cryptonator라는 오픈 API가 있다.(비트코인 API) Cryptonator 웹 사이트인데 이건 API는 아니다. 사람이 읽을 수 있는 방식으로 문서화한 것이다. HTML, CSS와 JavaScript로 만든 페이지다.
+
+// ========================================
+
+// < Cryptonator API > PublicAPI
+// https://publicapis.io/cryptonator-api/
+
+// < Cryptonator API >  public-apis.io
+// https://publicapi.dev/cryptonator-api
