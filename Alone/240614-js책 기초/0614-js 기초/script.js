52060b86cd3ad691639046db10222e7b3cfbbf3c
Ahnsj828 <ala828@naver.com>
2024-06-15 12:27:41 +0900

    240615

---
+++ Alone/240614-js책 기초/0614-js 기초/script.js
@@ -0,0 +1,31 @@
+document.write("환영합니다")
+// document.body.innerHTML= "<br/>"
+document.write("hi")
+
+document.write("환영합니다2<br/>")
+// document.body.innerHTML= "<br/>"
+document.write("hi2")
+
+document.write("<br/>")
+// =========================================
+document.write("<br/>")
+
+const booleanA = true
+const booleanB = false
+const booleanC = 10 > 5
+const booleanD = Boolean(null)
+
+document.write(booleanA, "<br/>")
+document.write(booleanB, "<br/>")
+document.write(booleanC, "<br/>")
+document.write(booleanD, "<br/>")
+
+document.write("<br/>")
+// =========================================
+document.write("<br/>")
+
+const numA = 100
+const strA = "Javascript"
+document.write(typeof numA, "<br/>")
+document.write(typeof strA, "<br/>")
+
