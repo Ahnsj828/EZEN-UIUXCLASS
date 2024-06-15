ef1ad9bd8d37f90e181b3c26747141913720198f
Ahnsj828 <ala828@naver.com>
2024-06-15 13:10:11 +0900

    240615

---
+++ Alone/240614-js책 기초/0614-js 기초3 - 증감연산자/script.js
@@ -0,0 +1,27 @@
+let num1 = 10
+let num2 = 20
+let result
+document.write("let num1 = ", num1, "<br/>") // let num1 = 10
+document.write("let num2 = ", num2, "<br/>") // let num2 = 20
+document.write("let result", "<br/>") // let result
+document.write("<br/>")
+
+num1-- // 10 - 1 = 9
+document.write("num1--", "<br/>") 
+document.write("num1 = ", num1, "<br/>") // num1 = 9
+
+document.write("<br/>")
+
+num1++ // 9 + 1 = 10
+document.write("num1--", "<br/>") 
+document.write("num1 = ", num1, "<br/>") // num1 = 10
+
+document.write("<br/>")
+
+result = num2++ // result = 20
+document.write(result, "<br/>") // 20
+document.write("result = ", result, "<br/>") // num1 = 10
+
+document.write("<br/>")
+// =========================================
+document.write("<br/>")
