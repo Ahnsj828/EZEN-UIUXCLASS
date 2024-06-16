7cedbcb4542d045383eadababbee4607973e272c
Ahnsj828 <ala828@naver.com>
2024-06-16 22:29:56 +0900

    240616

---
+++ Alone/240615-js책 기초/240615-js 기초5 - 논리연산자/script.js
@@ -0,0 +1,13 @@
+document.write("||", "<br/>")
+document.write("or연산자라 부르며, 피연산자 중 값이 하나라도 ture가 존재하면 true로 결괏값을 반환한다", "<br/>")
+
+document.write("&&", "<br/>")
+document.write("and연산자라 부르며, 피연산자 중 값이 하나라도 false가 존재하면 false로 결괏값을 반환한다", "<br/>")
+
+document.write("!", "<br/>")
+document.write("not연산자라 부르며, 피연산자의 값이 true이면 반대로 false로 결괏값을 반환한다", "<br/>")
+document.write("not연산자라 부르며, 피연산자의 값이 false이면 반대로 true로 결괏값을 반환한다", "<br/>")
+
+document.write("<br/>")
+document.write("============================================")
+document.write("<br/>")
